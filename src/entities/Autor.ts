37933709eb4d2c17d17af7510bd99763e4f96a9f
p-skipper <kipperspedro@gmail.com>
2025-01-17 13:36:27 -0300

    feat: create author entity

--- src/entities/Autor.ts
+++ src/entities/Autor.ts
@@ -1,5 +1,34 @@
-class Autor {
-    
-}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-export default Autor;
+@Entity("authors")
+export class Autor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  birthdate: Date;
+
+  @Column({ nullable: true })
+  biography: string;
+
+  @Column()
+  nationality: string;
+
+  @Column({ default: true })
+  active: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
